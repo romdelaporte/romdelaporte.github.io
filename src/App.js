4363721805528b317d6c76426ed90a417b13527f
romdelaporte <rodrigod@delaportedigital.com>
2024-12-28 21:59:50 -0300

    UI

---
+++ src/App.js
@@ -0,0 +1,17 @@
+import logo from './logo.svg';
+import './App.css';
+import './output.css';
+import { NavBar } from './components/navbar';
+import { Gallery } from './components/gallery';
+
+function App() {
+  
+  return (
+    <>
+    <NavBar />
+    <Gallery />
+    </>
+  );
+}
+
+export default App;
